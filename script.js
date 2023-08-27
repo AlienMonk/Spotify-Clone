3fdb70d95c65bebd851f6fe1e5beacf5f8eca9f3
AlienMonk <mklamba2001@gmail.com>
2023-08-27 20:06:43 +0530

    Added songes added their covers and time stamps

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let songIndex=0;
 let audioElement=new Audio('songs/1.mp3');
 let masterPlay =document.getElementById('masterPlay');
 let myProgressBar =document.getElementById('myProgressBar');
+let gif =document.getElementById('gif');
 let songs=[
     {songName:"Salam-e-ishq", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
     {songName:"Salam-e-ishq", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
@@ -19,14 +20,20 @@ masterPlay.addEventListener('click',()=>{
         audioElement.play();
         masterPlay.classList.remove('fa-play-circle');
         masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity=1;
     }
     else{
         audioElement.pause();
         masterPlay.classList.remove('fa-pause-circle');
         masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity=0;
     }
 })
 
-myProgressBar.addEventListener('timeupdate',()=>{
+audioElement.addEventListener('timeupdate',()=>{
     console.log(('timeupdate'));
+    progress=parseInt((audioElement.currentTime/audioElement.duration)*100);
+    console.log(progress);
+    myProgressBar.value=progress;
 })
+
